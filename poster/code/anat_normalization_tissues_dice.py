542b51324f7672876c1acbca114bbffaf4e89b37
Salma BOUGACHA <salmabougacha@hotmail.com>
2018-06-07 14:49:30 +0200

    normalize to reoriented version

--- poster/code/anat_normalization_tissues_dice.py
+++ poster/code/anat_normalization_tissues_dice.py
@@ -7,6 +7,8 @@ import os
 import numpy as np
 from nilearn import image
 from nilearn._utils.niimg_conversions import check_niimg
+from sammba.externals.nipype.interfaces import afni
+from sammba.externals.nipype.utils.filemanip import fname_presuffix
 
 
 def dice(mask_file1, mask_file2):
@@ -18,36 +20,62 @@ def dice(mask_file1, mask_file2):
 
 if __name__ == '__main__':
     template_dir = os.path.join('/home/Pmamobipet/0_Dossiers-Personnes/Salma',
-                                'inhouse_mouse_perf/template/with_mask')
-    registered_dir = os.path.join('/home/Pmamobipet/0_Dossiers-Personnes',
-                                  'Salma/inhouse_mouse_perf_preprocessed_mine',
-                                  'mouse_191851/reoriented')
+                                'templates/mouse/reoriented')
+    template_file = os.path.join(template_dir, 'head100.nii')
+    template_brain_mask_file = os.path.join(template_dir,
+                                            'brain100_binarized.nii')
+    spm_dir = os.path.join('/home/Pmamobipet/0_Dossiers-Personnes',
+                           'Salma/inhouse_mouse_perf',
+                           'mouse_160257/reoriented')
+    sammba_dir = os.path.join('/home/bougacha',
+                              'inhouse_mouse_perf_to_reoriented_head100',
+                              '160257')
 
     template_tissues_imgs = [
         os.path.join(template_dir, 'c{0}head100.nii'.format(n))
         for n in range(1, 4)]
-    template_mask_img = image.math_img(
-        'np.max(imgs, axis=-1) > .000001', imgs=template_tissues_imgs)
-    template_img = image.math_img(
+    template_labels_img = image.math_img(
         'img * (np.argmax(imgs, axis=-1) + 1)',
-        img=template_mask_img,
+        img=template_brain_mask_file,
         imgs=template_tissues_imgs)
 
-    registered_tissues_imgs = [
-        os.path.join(
-            registered_dir,
-            'c{0}anat_n0_unifized_affine_general_warped_clean_hd.nii'.format(n))
-        for n in range(1, 4)]
-    registered_mask_img = image.math_img(
-        'np.max(imgs, axis=-1) > .000001', imgs=registered_tissues_imgs)
-    registered_img = image.math_img(
-        'img * (np.argmax(imgs, axis=-1) + 1)',
-        img=registered_mask_img,
-        imgs=registered_tissues_imgs)
+    spm_labels_files = [
+        os.path.join(output_dir, prefix + 'anat_n0_clear_hd_labeled.nii.gz')
+        for (prefix, output_dir) in zip(['wc', 'c'], [spm_dir, sammba_dir])]
+    for (prefix, spm_labels_file) in zip(['wc', 'c'], spm_labels_files):
+        spm_tissues_imgs = [
+            os.path.join(
+                spm_dir, prefix + '{0}anat_n0_clear_hd.nii'.format(n))  # Try after N3
+            for n in range(1, 4)]
+        if prefix == 'c':
+            mask_img = image.math_img(
+                'np.max(imgs, axis=-1) > .01', imgs=spm_tissues_imgs)
+        else:
+            mask_img = template_brain_mask_file
+        spm_labels_img = image.math_img(
+            'img * (np.argmax(imgs, axis=-1) + 1)',
+            img=mask_img,
+            imgs=spm_tissues_imgs)
+        spm_labels_img.to_filename(spm_labels_file)
 
-    for label in [1, 2, 3]:
-        tissue_template_img = image.math_img('img=={0}'.format(label),
-                                             img=template_img)
-        tissue_registered_img = image.math_img('img=={0}'.format(label),
-                                               img=registered_img)
-        print(dice(tissue_template_img, tissue_registered_img))
+
+    nwarp_apply = afni.NwarpApply().run
+    transforms = ['anat_n0_unifized_affine_general_warped_WARP.nii.gz',
+    'anat_n0_unifized_masked_aff.aff12.1D']
+    warp = "'" + ' '.join(transforms) + "'"
+    sammba_labels_file = fname_presuffix(spm_labels_file,
+                                         suffix='_warped',
+                                         newpath=sammba_dir)
+    out_warp_apply = nwarp_apply(in_file=spm_labels_files[1],
+                                 master=template_file,
+                                 warp=warp,
+                                 ainterp='nearestneighbor',
+                                 out_file=sammba_labels_file)
+
+    for labels_file in [spm_labels_files[0], sammba_labels_file]:
+        for label in [1, 2, 3]:
+            tissue_template_img = image.math_img('img=={0}'.format(label),
+                                                 img=template_labels_img)
+            tissue_registered_img = image.math_img('img=={0}'.format(label),
+                                                   img=labels_file)
+            print(dice(tissue_template_img, tissue_registered_img))
--- poster/code/normalize_perfs.py
+++ poster/code/normalize_perfs.py
@@ -16,22 +16,27 @@ m0_files = [os.path.join(d, 'perfFAIREPI_n0_M0.nii.gz') for d in mice_dirs]
 perf_proc_files = [os.path.join(d, 'perfFAIREPI_n0_proc.nii.gz')
                    for d in mice_dirs]
 
-template_file = os.path.join(
-    '/home/Promane/2014-ROMANE/5_Experimental-Plan-Experiments-Results/mouse',
-    'MRIatlases/MIC40C57/20170223/head100.nii.gz')
-template_brain_mask_file = os.path.join('/home/bougacha/inhouse_mouse',
-                                        'brain100_binarized.nii.gz')
+output_dir = os.path.join('/home/bougacha',
+                          'inhouse_mouse_perf_to_reoriented_head100')
+template_file = os.path.join(output_dir, 'head100_reoriented.nii.gz')
+
+template_brain_mask_file = os.path.join(output_dir,
+                                        'brain100_reoriented_binarized.nii.gz')
+
+if not os.path.isfile(template_brain_mask_file):
+    template_brain_file = os.path.join(output_dir,
+    'brain100_reoriented.nii.gz')
+    img = image.math_img('img>0', img=template_brain_file)
+    img.to_filename(template_brain_mask_file)   
+    
 spm_data_dir = os.path.join('/home/Pmamobipet/0_Dossiers-Personnes/Salma/',
-                            'inhouse_mouse_perf_preprocessed_mine')
+                            'inhouse_mouse_perf_sammba')
 for mouse_dir, anat_file, m0_file, perf_proc_file in zip(mice_dirs,
                                                          anat_files,
                                                          m0_files,
-                                                         perf_proc_files)[2:]:
-    mouse_id = os.path.basename(mouse_dir)
-    mouse_id = mouse_id.replace('20170104', 'mouse')
-    mouse_id = mouse_id.replace('_MINDt_ROMANE_56634_1_1', '')
-    output_dir = os.path.join(
-        '/home/bougacha/inhouse_mouse_perf_preprocessed_mine', mouse_id)
+                                                         perf_proc_files)[4:]:
+    mouse_id = os.path.basename(mouse_dir)[9:15]
+    output_dir = os.path.join(output_dir, mouse_id)
     registrator = template_registrator.TemplateRegistrator(
         brain_volume=400,
         dilated_template_mask=None,
@@ -41,17 +46,17 @@ for mouse_dir, anat_file, m0_file, perf_proc_file in zip(mice_dirs,
 
     registrator.fit_anat(anat_file)
     # Save uncompressed image to output spm directory
-    target_dir = os.path.join(spm_data_dir, mouse_id)
-    if not os.path.isdir(target_dir):
-        os.makedirs(target_dir)
-    target_anat_base, _ = os.path.splitext(
-        os.path.basename(registrator.registered_anat))
-    registered_anat_img = nibabel.load(registrator.registered_anat)
-    registered_anat_img.to_filename(os.path.join(target_dir, target_anat_base))
+#    target_dir = os.path.join(spm_data_dir, mouse_id)
+#    if not os.path.isdir(target_dir):
+#        os.makedirs(target_dir)
+#    target_anat_base, _ = os.path.splitext(
+#        os.path.basename(registrator.registered_anat))
+#    registered_anat_img = nibabel.load(registrator.registered_anat)
+#    registered_anat_img.to_filename(os.path.join(target_dir, target_anat_base))
 
     registrator.fit_modality(m0_file, 'perf')
-    cbf_file = os.path.join(output_dir, 'perfFAIREPI_n0_cbf.nii.gz')
-    cbf = image.index_img(perf_proc_file, 13)
-    cbf.to_filename(cbf_file)
+#    cbf_file = os.path.join(output_dir, 'perfFAIREPI_n0_cbf.nii.gz')
+#    cbf = image.index_img(perf_proc_file, 13)
+#    cbf.to_filename(cbf_file)
 
-    normalized_cbf = registrator.transform_modality_like(cbf_file, 'perf')
+ #   normalized_cbf = registrator.transform_modality_like(cbf_file, 'perf')
